78436ea63c7856023096f35519497e5db32c8b3e
Luv Agnihotri <luvagnihotri07@gmail.com>
2025-12-09 12:18:22 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,14 @@ import NotFound from "./pages/NotFound";
 import Settings from "./pages/Settings";
 import Help from "./pages/Help";
 
+// NEW PAGES - Import these
+import CropDisease from "./pages/CropDisease";
+import SoilAnalysis from "./pages/SoilAnalysis";
+import IoTDashboard from "./pages/IoTDashboard";
+import Weather from "./pages/Weather";
+import MarketPrices from "./pages/MarketPrices";
+import CropRecommendation from "./pages/CropRecommendation";
+
 const queryClient = new QueryClient();
 
 const App = () => (
@@ -20,7 +28,15 @@ const App = () => (
           <Route path="/" element={<Index />} />
           <Route path="/settings" element={<Settings />} />
           <Route path="/help" element={<Help />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          
+          {/* MODULE ROUTES - Add these */}
+          <Route path="/crop-disease" element={<CropDisease />} />
+          <Route path="/soil-analysis" element={<SoilAnalysis />} />
+          <Route path="/iot-dashboard" element={<IoTDashboard />} />
+          <Route path="/weather" element={<Weather />} />
+          <Route path="/market-prices" element={<MarketPrices />} />
+          <Route path="/crop-recommendation" element={<CropRecommendation />} />
+          
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
